c6808f3c53e7a5a25dd735e0493ef32dae8b1ada
Crediteur <74624064+Crediteur@users.noreply.github.com>
2023-03-06 19:21:01 -0500

    Updates

--- _app/immutable/chunks/1.6b32b67a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.d8999625.js";export{t as component};
---
+++ _app/immutable/chunks/3.1db2beed.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/about-page.svelte.9a262896.js";export{t as component};
